89d348a122fb771c9e3c28a11f9a763288000a3c
orange-mush <greatSumini@gmail.com>
2020-10-13 04:05:33 +0900

    feat: :mag: add site meta data

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const siteTitle = '김보창 트리';
+  const siteUrl = 'https://sexy-bochang.now.sh/';
+
+  return (
+    <>
+      <Head>
+        <title>{siteTitle}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="shortcut icon"
+          type="image/png"
+          href="/images/og.png"
+          key="favicon"
+        />
+        <meta property="og:title" content={siteTitle} />
+        <link rel="canonical" href={siteUrl} />
+        <meta property="og:url" content={siteUrl} />
+        <meta property="og:image" content="/images/og.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
