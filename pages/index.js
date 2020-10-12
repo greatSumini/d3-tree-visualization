1a61a1d0f7c84a3129853d4f3523df2e6fe05e73
orange-mush <greatSumini@gmail.com>
2020-10-13 03:27:14 +0900

    feat: :sparkles: add Home

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,113 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { useState } from 'react';
+import styled from 'styled-components'
+import * as d3 from "d3";
+
+const WIDTH = 954;
+const RADIUS = WIDTH / 2;
+
+const tree = d3.tree()
+  .size([2 * Math.PI, RADIUS])
+  .separation((a, b) => (a.parent == b.parent ? 1 : 2) / a.depth);
 
 export default function Home() {
+  const [inputFile, setInputFile] = useState(null);
+  const [expandRate, setExpandRate] = useState(1);
+
+  const loadFile = (event) => {
+    const reader = new FileReader();
+    reader.onload = (sender) => {
+      const result = JSON.parse(sender.target.result.replace(/u'(?=[^:]+')/g, "'"));  
+      setInputFile(result);
+    }
+    reader.readAsText(event.target.files[0]);
+  }
+
+  const render = () => {
+    if(!inputFile) {
+      alert('파일이 업로드되지 않았습니다.');
+      return;
+    }
+
+    const data = d3.hierarchy(inputFile).sort((a, b) => d3.ascending(a.data.name, b.data.name));
+    const root = tree(data);
+
+    const svg = d3.select('#svg');
+
+    console.log(root, svg);
+    
+ svg.append("g")
+      .attr("fill", "none")
+      .attr("stroke", "#555")
+      .attr("stroke-opacity", 0.4)
+      .attr("stroke-width", 1.5)
+    .selectAll("path")
+    .data(root.links())
+    .join("path")
+      .attr("d", d3.linkRadial()
+          .angle(d => d.x)
+          .radius(d => d.y));
+  
+  svg.append("g")
+    .selectAll("circle")
+    .data(root.descendants())
+    .join("circle")
+      .attr("transform", d => `
+        rotate(${d.x * 180 / Math.PI - 90})
+        translate(${d.y},0)
+      `)
+      .attr("fill", d => d.children ? "#555" : "#999")
+      .attr("r", 2.5);
+
+  svg.append("g")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", 10)
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-width", 3)
+    .selectAll("text")
+    .data(root.descendants())
+    .join("text")
+      .attr("transform", d => `
+        rotate(${d.x * 180 / Math.PI - 90}) 
+        translate(${d.y},0) 
+        rotate(${d.x >= Math.PI ? 180 : 0})
+      `)
+      .attr("dy", "0.31em")
+      .attr("x", d => d.x < Math.PI === !d.children ? 6 : -6)
+      .attr("text-anchor", d => d.x < Math.PI === !d.children ? "start" : "end")
+      .text(d => d.data.name)
+    .clone(true).lower()
+      .attr("stroke", "white");
+
+      svg.attr("viewBox", autoBox).node();
+  }
+    
+  const autoBox = () => {
+    const ele = document.getElementById('svg')
+    const {x, y, width, height} = ele.getBBox();
+    return [x, y, width, height];
+  }
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+    <Wrapper>
+      <StyledLabel htmlFor='file'>upload file</StyledLabel><StyledInput id='file' type='file' accept=".json" onChange={loadFile}/>
+      <StyledLabel htmlFor='number'>expandRate</StyledLabel><StyledInput id='number' type="number" value={expandRate} onChange={(e) => {setExpandRate(e.target.value)}}/>
+      <button onClick={render}>render</button>
+      <button onClick={render}>export png</button>
+      <svg id='svg' />
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`
+
+const StyledLabel = styled.label`
+  font-weight: 700;
+`
+
+const StyledInput = styled.input`
+  margin-bottom: 2rem;
+`
